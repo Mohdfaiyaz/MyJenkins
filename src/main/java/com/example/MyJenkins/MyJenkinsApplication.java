e2e6da4959e6f9e033092091e4004e95915156ac
Mohd Faiyaz Uddin <faiyaz@git.com>
2020-11-12 15:11:00 +0530

    First Commit

---
+++ src/main/java/com/example/MyJenkins/MyJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyJenkins/MyJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
